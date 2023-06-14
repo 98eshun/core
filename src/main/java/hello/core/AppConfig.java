6f2ffe9f370eca43dd6aacd7e65c4b7b322793da
S_H_Lee <dabansa2@naver.com>
2023-06-14 14:55:00 +0900

    section 5

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,16 +15,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();    // DB 정책이 바뀌면 이 코드만 변경하면 된다.
     }
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     @Bean
